d4f850c0fec80987a0a0648ed56545bd19d3358a
Caspar Clemens Mierau <ccm@screenage.de>
2013-11-05 15:37:35 +0100

    adding postinstall with puppet dpkg files

--- custom-postinstall.sh
+++ custom-postinstall.sh
@@ -2,3 +2,9 @@
 # you should keep in mind, that this script is run inside a chrooted
 # envorinment of the freshly installed server, so every change here
 # is a direct change in the new server
+
+apt-get -y install byobu wget
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstrap/basenode/root-ssh-authorizedkeys.pub
+bash < <(wget -qO - https://bit.ly/install-puppet-agent)
