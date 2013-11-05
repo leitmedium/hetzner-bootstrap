2efe22cf45f3f0728e9320a4c82a6e9562b8b827
Caspar Clemens Mierau <ccm@screenage.de>
2013-11-05 15:46:55 +0100

    adding missing files

---
+++ hetzner-installimage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://raw.github.com/leitmedium/hetzner-bootstrap/mbr-basenode/hetzner-postinstall.sh
+chmod 755 hetzner-postinstall.sh
+echo "${SERVERNAME}" | egrep '^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$' || VALIDHOSTNAME="false"
+if [[ -n "$VALIDHOSTNAME" ]]; then
+  echo "the provided SERVERNAME '${SERVERNAME}' is either invalid or empty"
+  exit 1
+else
+  /root/.oldroot/nfs/install/installimage -a -p /boot:ext3:1G,/:ext4:all -r yes -l 1 -b grub -d sda,sdb -i /root/.oldroot/nfs/install/../images/Ubuntu-1204-precise-64-minimal.tar.gz -x hetzner-postinstall.sh -n ${SERVERNAME}
+fi
---
+++ hetzner-postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get -y install byobu wget
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.23-1puppetlabs1_all.deb
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.23-1puppetlabs1_all.deb
+dpkg -i puppet-common_2.7.23-1puppetlabs1_all.deb
+dpkg -i puppet_2.7.23-1puppetlabs1_all.deb
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+wget -O /root/.ssh/authorized_keys https://raw.github.com/leitmedium/hetzner-bootstrap/mbr-basenode/root-ssh-authorizedkeys.pub
+# please add you custom postinstall commands here
+# you should keep in mind, that this script is run inside a chrooted
+# envorinment of the freshly installed server, so every change here
+# is a direct change in the new server
